78321bfe9dfc886ed031b1f34be188391cddae8f
Erika La <henriikka.laurola@gmail.com>
2019-07-16 23:39:01 +0200

    added custom queries

--- page-about.php
+++ page-about.php
@@ -32,15 +32,41 @@ $size = "full";
 ?>
 
 <div id="hero-text">
-						<p><span class="company-name">Accelerate</span> <?php echo $hero_text; ?></p>
-					</div><!-- #hero-statement -->
+						<h3><?php echo $hero_text; ?><h3>
+					</div><!-- #hero-text -->
 				</div><!-- #aboutpage-hero -->
-					<?php endwhile; // end of the loop. ?>
+  <?php endwhile; // end of the loop. ?>
 
-<article class="services">
-    <aside class="services-sidebar">
+<article class="about-services">
+      <div id="services">
+      		<h4><?php echo $page_title; ?></h4>
+      			<p><?php echo $page_description; ?></p>
+      </div>
 
+      <div id="content-strategy">
+      		<div class="content-text">
+      				<h5><?php echo $service_title_1; ?></h5>
+      					 <p><?php echo $service_description_1; ?><p>
+      		</div>
+      		<?php if ($icon_1) {
+      							echo wp_get_attachment_image( $icon_1, $size );
+      					} ?>
+      </div>
 
-	</div><!-- #primary -->
+      <div id="influencer-mapping">
+          <div class="influencer-text">
+              <h5><?php echo $service_title_2; ?></h5>
+                  <p><?php echo $service_description_2; ?><p>
+          </div>
+          <?php if ($icon_2) {
+                    echo wp_get_attachment_image( $icon_2, $size );
+                    } ?>
+          </div>
+          
+  </article>
+
+    </div><!-- .main-content -->
+
+   </div><!-- #primary -->
 
 <?php get_footer(); ?>
