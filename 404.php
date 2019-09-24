face1bc2075756bdd7b0e991093a54d202df5002
Erika La <henriikka.laurola@gmail.com>
2019-09-24 23:15:09 +0200

    added a 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the php page.
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+   <div id="404-content">
+     <h4>Whoops, took a wrong turn...</h4>
+     <p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for misleading you.</p>
+     <p>Feel free to take a look around our blog or some of our featured work.</p>
+   </div>
+<?php get_footer(); ?>
